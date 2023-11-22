4dc149249acbe70ae7efdc25c5878f53534146c2
ZooM <yuhos@mail.ru>
2023-11-22 17:15:01 +0300

    Get some problems with aliquot

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double afterTaxes;
+        if (salary < 0) {
+            System.out.println("wrong input!");
+        } else if (salary <= 10000) {
+            afterTaxes = salary - salary * 0.15;
+            System.out.println(afterTaxes);
+        } else if (salary <= 20000) {
+            afterTaxes = salary - salary * 0.18;
+            System.out.println(afterTaxes);
+        } else {
+            afterTaxes = salary - salary * 0.20;
+            System.out.println(afterTaxes);
+        }
     }
 }
